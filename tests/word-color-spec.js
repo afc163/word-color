a75c22c13e5abca180b6bf3503e8048d8d01ea86
Hsiaoming Yang <me@lepture.com>
2015-06-19 22:29:57 +0800

    Add tests for rgb API

--- tests/word-color-spec.js
+++ tests/word-color-spec.js
@@ -5,6 +5,7 @@ describe('word-color', function () {
 
   it('normal usage', function () {
     expect(wordColor('words')).to.be('rgb(188,174,18)');
+    expect(wordColor.rgb('words')).to.be([188,174,18]);
   });
 
   it('long words', function () {
